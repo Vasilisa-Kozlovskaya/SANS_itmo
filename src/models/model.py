989c4f177deb87ab1243c61734bffa4b50af5d02
Vasilisa <akademydeski@gmail.com>
2026-06-25 01:02:05 +0300

    update

--- src/models/model.py
+++ src/models/model.py
@@ -111,7 +111,6 @@ class GPT(pl.LightningModule):
     
     # Рассчитываем общее количество шагов обучения (нужно для планировщика)
     # Эти данные мы подтянем из трейнера позже или зададим примерно
-        steps_per_epoch = len(self.trainer.train_dataloader)
         total_steps = self.config.training.get('total_steps', 10000)
         
         scheduler = get_cosine_schedule_with_warmup(
